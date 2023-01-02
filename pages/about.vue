8062ce5ab6b3da10bfc0d5a41795c35280972f7d
edwardz8 <tolklorist@gmail.com>
2023-01-02 15:27:22 -0500

    init

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ion-page>
+    <div>About page</div>
+  </ion-page>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ion-page>
+    <div>Hello from index page</div>
+  </ion-page>
+</template>
